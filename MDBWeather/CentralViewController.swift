63f1164f7e94cdbf7fea411d852a456f31baf0e3
Candice Ye <Candice@airbears2-10-142-65-116.airbears2.1918.berkeley.edu>
2016-10-15 22:35:49 -0700

    final commit!

--- MDBWeather/CentralViewController.swift
+++ MDBWeather/CentralViewController.swift
@@ -5,14 +5,12 @@
 //  Created by Candice Ye on 10/14/16.
 //  Copyright © 2016 Candice Ye. All rights reserved.
 //
-
 import UIKit
 import Alamofire
 import CoreLocation
 
-
 class CentralViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     // ALAMOFIRE VARIABLES - - - - -
     var locationURL = "https://api.darksky.net/forecast/639a5ac270e86e62a1d82629fa128e19/37.8267,-122.4233"
     
@@ -24,11 +22,9 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
     
     // LOCATION VARIABLES
     var locationManager = CLLocationManager()
+    // - - - - - - - - - - - - - - -
     
-    // - - -
     
-    
-
     // STATIC VARIABLES - - - - - -
     var backgroundImage: UIImageView!
     var logoImage: UIImageView!
@@ -43,24 +39,17 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
     var weatherDescription: UILabel!
     // - - - - - - - - - - - - - - -
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startLocation()
-
+        callAlamo(url: locationURL)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     
-    
-    
-    
-   //GET COORDINATES ---------------------------
-
-    func startLocation(){
+    //GET COORDINATES - - - - - - - - -
+    func startLocation() {
         if (CLLocationManager.locationServicesEnabled())
         {
             locationManager = CLLocationManager()
@@ -71,27 +60,21 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last! as CLLocation
         let latitude =  location.coordinate.latitude
         let longitude = location.coordinate.longitude
         manager.stopUpdatingLocation()
+        
         var locationURL = "https://api.darksky.net/forecast/639a5ac270e86e62a1d82629fa128e19/ \(String(latitude)), \(String(longitude))"
-        callAlamo(url: locationURL)
     }
-    
-    // -----------------------------
-    
-    
-    
+    // - - - - - - - - - - - - - - -
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
     
     // ALAMOFIRE - - - - - - - - - -
@@ -128,24 +111,22 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         }
         
     }
-    
-    // - - - - - - - - - - - - - - -
+    // - - - - - - - - - - - - - - - - -
     
     
     // ACTIONS / FUNCTIONS - - - - - - -
     
-    
     func setUpCentral() {
         
-        
         // LOGO BACKGROUND
-        backgroundImage = UIImageView(image: #imageLiteral(resourceName: "goodBackground"))
+        backgroundImage = UIImageView(image: #imageLiteral(resourceName: "finalbackground"))
         backgroundImage.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         self.view.addSubview(backgroundImage)
         
+        
         // LOGO IMAGE
-        logoImage = UIImageView(image: #imageLiteral(resourceName: "rainCheckLogo3"))
-        logoImage.frame = CGRect(x: view.frame.width/8, y: 30, width: 0.75*(view.frame.width), height: view.frame.height/5)
+        logoImage = UIImageView(image: #imageLiteral(resourceName: "rainCheckLogo3-1"))
+        logoImage.frame = CGRect(x: view.frame.width/2 - view.frame.height/10 - 20, y: 20, width: view.frame.height/5 + 35, height: view.frame.height/5 + 35)
         logoImage.layer.masksToBounds = true
         logoImage.contentMode = .scaleAspectFit
         logoImage.layer.cornerRadius = 10
@@ -153,7 +134,7 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         
         // CREDIT
         credit = UIButton()
-        credit.frame = CGRect(x: view.frame.width/4, y: 40+view.frame.height/5, width: view.frame.width/2, height: 22)
+        credit.frame = CGRect(x: view.frame.width/4, y: 40+view.frame.height/5 + 5, width: view.frame.width/2, height: 22)
         credit.setTitle("Powered by DarkSky", for: .normal)
         credit.setTitleColor(UIColor.white, for: .normal)
         credit.addTarget(self, action:#selector(urlMaker), for: .touchUpInside)
@@ -185,7 +166,7 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         
         // RAIN CHANCE
         rainChance = UILabel()
-        rainChance.frame = CGRect(x: view.frame.width*0.1, y: view.frame.height/2 + 10, width: view.frame.width*0.8, height: view.frame.height/8)
+        rainChance.frame = CGRect(x: view.frame.width*0.1, y: view.frame.height/2 + 25, width: view.frame.width*0.8, height: view.frame.height/8)
         rainChance.text = "Chance of rain: \(Int(precipProbability!*100))%"
         rainChance.textColor = UIColor.white
         rainChance.font = UIFont(name: "Avenir", size: 20)
@@ -194,7 +175,7 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         
         // RAIN DETAILS
         rainDetails = UILabel()
-        rainDetails.frame = CGRect(x: view.frame.width*0.1, y: view.frame.height/2 + 35, width: view.frame.width*0.8, height: view.frame.height/8)
+        rainDetails.frame = CGRect(x: view.frame.width*0.1, y: view.frame.height/2 + 50, width: view.frame.width*0.8, height: view.frame.height/8)
         rainDetails.text = minutelySummary!
         rainDetails.textColor = UIColor.white
         rainDetails.font = UIFont(name: "Avenir", size: 20)
@@ -204,7 +185,7 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         
         // DESCRIPTION / SUMMARY
         weatherDescription = UILabel()
-        weatherDescription.frame = CGRect(x: (view.frame.width)*0.1, y: view.frame.height*(1/2), width: view.frame.width*0.8, height: (view.frame.height/3))
+        weatherDescription.frame = CGRect(x: (view.frame.width)*0.1, y: view.frame.height*(1/2) + 15, width: view.frame.width*0.8, height: (view.frame.height/3))
         weatherDescription.lineBreakMode = NSLineBreakMode.byWordWrapping
         weatherDescription.numberOfLines = 0
         weatherDescription.textColor = UIColor.white
@@ -215,11 +196,10 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
         weatherDescription.minimumScaleFactor = 0.5
         weatherDescription.adjustsFontSizeToFitWidth = true
         self.view.addSubview(weatherDescription)
-        
     }
- 
     
-    // HYPERLINK
+    
+    // HYPERLINK - - - - - - - - - - -
     func urlMaker(sender: UIButton) {
         let url = URL(string: "https://darksky.net/poweredby/")
         if #available(iOS 10.0, *) {
@@ -228,8 +208,6 @@ class CentralViewController: UIViewController, CLLocationManagerDelegate {
             UIApplication.shared.openURL(url!)
         }
     }
-    
-    
     // - - - - - - - - - - - - - - - - -
-
+    
 }
